5a494d9d5993fe7e47a7578d1b27b25b22bf8614
uma-victor1 <umavictor11@gmail.com>
2025-01-15 00:31:52 +0100

    first commit

---
+++ components/Dropdowns/IndexDropdown.js
@@ -0,0 +1,147 @@
+import React from "react";
+import Link from "next/link";
+import { createPopper } from "@popperjs/core";
+
+const IndexDropdown = () => {
+  // dropdown props
+  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+  const btnDropdownRef = React.createRef();
+  const popoverDropdownRef = React.createRef();
+  const openDropdownPopover = () => {
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: "bottom-start",
+    });
+    setDropdownPopoverShow(true);
+  };
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+  return (
+    <>
+      <a
+        className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+        href="#pablo"
+        ref={btnDropdownRef}
+        onClick={(e) => {
+          e.preventDefault();
+          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+        }}
+      >
+        Demo Pages
+      </a>
+      <div
+        ref={popoverDropdownRef}
+        className={
+          (dropdownPopoverShow ? "block " : "hidden ") +
+          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+        }
+      >
+        <span
+          className={
+            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
+          }
+        >
+          Admin Layout
+        </span>
+        <Link href="/admin/dashboard">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Dashboard
+          </p>
+        </Link>
+        <Link href="/admin/settings">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Settings
+          </p>
+        </Link>
+        <Link href="/admin/tables">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Tables
+          </p>
+        </Link>
+        <Link href="/admin/maps">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Maps
+          </p>
+        </Link>
+        <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
+        <span
+          className={
+            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
+          }
+        >
+          Auth Layout
+        </span>
+        <Link href="/auth/login">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Login
+          </p>
+        </Link>
+        <Link href="/auth/register">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Register
+          </p>
+        </Link>
+        <div className="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
+        <span
+          className={
+            "text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
+          }
+        >
+          No Layout
+        </span>
+        <Link href="/landing">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Landing
+          </p>
+        </Link>
+        <Link href="/profile">
+          <p
+            href="#pablo"
+            className={
+              "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            }
+          >
+            Profile
+          </p>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default IndexDropdown;
